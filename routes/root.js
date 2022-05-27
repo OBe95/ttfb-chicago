414122a7b3e447e2467df14327850f83301a6ba7
Richard Brus <richard@seoworx.net.au>
2022-05-27 21:02:00 +1000

    Fresh push

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const {checkTimings} = require('../controllers/puppeteer-check')
+const checkTimings = require('../controllers/puppeteer-check')
 router = express.Router()
 
 // User account page
-router.post('/check', checkTimings)
+router.post('/check', checkTimings.checkTimings)
 router.get('/', (req, res) => { res.send('Should be fine') })
 
 module.exports = router
