2102fdd5f5a219685e450cf3f705de12bc57138a
Richard Brus <richard@seoworx.net.au>
2022-05-28 20:40:23 +1000

    Fresh push

--- controllers/puppeteer-check.js
+++ controllers/puppeteer-check.js
@@ -77,7 +77,7 @@ const checkTimings = async (req, res) => {
 
     } catch (err) {
         console.log('err', err)
-        return res.sendStatus(401)
+        return res.json({error: JSON.stringify(err)})
 
     } finally {
         // Log checks
